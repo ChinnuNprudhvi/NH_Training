0b0974701956dfcfac2bf52da4cf38ce4fee6153
ChinnuNprudhvi <57140559+ChinnuNprudhvi@users.noreply.github.com>
2022-01-25 11:47:01 +0530

    Add files via upload

---
+++ Day1 Morning assignment by nalli_prudhvi 24 Jan 2022/Day_1_Project_2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day_1_Project_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = 1;
+            for(int i = 1; i <= b; i++)
+            {
+                c = c * a;
+            }
+            textBox4.Text = c.ToString();   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
