c4e75c1bef902211243d8f07b147065ff327d897
ChinnuNprudhvi <57140559+ChinnuNprudhvi@users.noreply.github.com>
2022-02-14 23:20:08 +0530

    Add files via upload

---
+++ Day16 assignment by prudhvi/PublicLibrarry/FactorialofNUm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicLibrarry
+{
+    public class FactorialofNUm
+    {
+        public int Fact(int z)
+        {
+            if (z <= 1)
+            {
+
+                return 1;
+            }
+            else
+            {
+                return z*Fact(z-1);
+            }
+        }
+    }
+}
---
+++ Day16 assignment by prudhvi/pHYSICs/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pHYSICs
+{
+    public class Phsics
+    {
+        public void Power(int v , int i)
+        {
+            Console.WriteLine( $"power = {v*i}watts");
+        }
+        public void voltage(int i, int r)
+        {
+            Console.WriteLine($"power = {r * i}watts");
+        }
+        public void current(int v, int r)
+        {
+            Console.WriteLine($"current = {v/r}");
+        }
+    }
+}
