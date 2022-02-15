7fc249217c6045c6b12ca4dc881b86a624e10ce2
ChinnuNprudhvi <57140559+ChinnuNprudhvi@users.noreply.github.com>
2022-02-15 22:57:34 +0530

    Add files via upload

---
+++ Day17 assignment by prudhvi/ASsemble1/Parent_c.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASsemble1
+{
+    public class Parent_c
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        /// <summary>
+        /// Base Class Method
+        /// </summary>
+        public void BaseClassMethod()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+    }
+    /// <summary>
+    /// Derived Class
+    /// </summary>
+    public class Child_c : Parent_c
+    {
+        /// <summary>
+        /// Derived Class Method
+        /// </summary>
+        public void DerivedClassMethod()
+        {
+            a = 10;
+            //b = 20; Private variable cannot accessble in same derived class
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+    }
+    /// <summary>
+    /// Main Class
+    /// </summary>
+    public class MainClass
+    {
+        /// <summary>
+        /// Base Class Object Created
+        /// </summary>
+        Child_c bc = new Child_c();
+        /// <summary>
+        /// Main Class Method
+        /// </summary>
+        public void MainClassMethod()
+        {
+            bc.a = 10;
+            //bc.b = 20; Private variable cannot accessble in same main class
+            //bc.c = 30; Protected variable cannot accessble in same main class
+            bc.d = 40;
+            bc.e = 50;
+        }
+    }
+}
---
+++ Day17 assignment by prudhvi/Another_Lib/AnotherAssCl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASsemble1;
+
+namespace Another_Lib
+{
+    public class AnotherAssCl : Parent_c
+    {
+        public void AnotherClMet()
+        {
+            a = 10;
+            //b = 20; Private variable cannot accessble in other derived class
+            c = 30;
+            //d = 40; Internal variable cannot accessble in other derived class
+            e = 50;
+        }
+
+    }
+    public class OtherMain
+    {
+        Parent_c bc = new Parent_c();   
+        public void OtherMainClassMethod()
+        {
+            bc.a = 10;
+            //bc.b = 20; Private variable cannot accessble in other main class
+            //bc.c = 30; Protected variable cannot accessble in other main class
+            //bc.d = 40; Internal variable cannot accessble in other main class
+            //bc.e = 50; Protected Internal variable cannot accessble in other main class
+        }
+
+    }
+
+}
